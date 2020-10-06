6b26833b64cf109a57e00150b7f69534483a80d6
Afaq Ahmed Saqi <afaqahmad6296@gmail.com>
2020-10-07 00:28:07 +0500

    Initial Commit

---
+++ Programming fundamentals/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Programming fundamentals
+//
+//  Created by Afaq Ahmed on 07/10/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
