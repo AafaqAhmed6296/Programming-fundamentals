da9ee518ef86ed77de0d5fd35b7f553dbe8d2f5a
Afaq Ahmed Saqi <afaqahmad6296@gmail.com>
2020-10-08 02:45:09 +0500

    2nd Day lecture.

--- Programming fundamentals/main.cpp
+++ Programming fundamentals/main.cpp
@@ -10,59 +10,121 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     
-    /// Variable declaration
+    /// Escape sequences.
+    // \"
+    // \n
+    // \b
     
-    // Data types
-    // Permitive/default data types
-    // |
-    // - int -- 4 bytes
-    // - long int -- 8 byte
-    // - unsigned int - 2 bytes
-    // - float -- 4 bytes
-    // - char -- 1 byte
-    // - bool -- byte
-    // - double -- 8 byte
+    char name[20];
+
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Your entered \"" << name << "\"" ;
     
-    /// Rules for naming variable.
-    // |
-    // first letter cannot be numerical
-    // first letter can be _ or alphabet
-    // special characters are not allowed
-    // variable cannot contain spaces
-    // permitive can not be used.
+    ///  Logical operators
+    //   And (&&)
+    //   Or (||)
+    //   Not (!)
+    ///  Relational operators // comparision operator
+    //   less than (<) // 8 < 7 false
+    //   greater that (>) // 7 > 8 false
+    //   less than and equal (<=)
+    //   greater than or equal (>=)
+    //   is equal to (==) // 4==4 true
+    //   Ternary operator (:?)
     
-    int variable1; // allowed
-    // int 1variable; // not allowed
-    // int vari able // not allowed i.e. it cannot have space between the words
-    // int double // not allowed.
+    ///  Control Strucrture
+    //   Sequential control
+    //   conditional
+    //   loop
     
-    int variable2;  /// variable declaration
+    ///  Conditional
+    //   |
+    //   if statement
+    //   if else
+    //   if else-if
+    ///  Unconditional transfer of control
+    //   goto
     
-    variable2 = 10; /// variable intialization
+    ///  Syntax of if statement
     
-    /// << <- insertion operater
+//    if (condition) {
+        // s1
+        //s2
+//    }
     
-    cout << variable2 << endl; // printing on console
+//    if ( (4) > 5)
+//        cout << "Hello";
     
-    // "any thing between quotation is string"
+    ///  Syntax if else
+//    if (condition) {
+//
+//    }else {
+//
+//    }
     
-    cout << "Enter number: ";
-    /// >> -> extraction operator
-    cin >> variable1; // getting input from user from console
+//    if (4 < 5)
+//        cout << "HEllo";
+//    else
+//        cout << "Hello23";
     
-    cout << "Variable 1: " << variable1 << " Variable 2: " << variable2 << endl;
+    ///  Syntax if else if
     
-    /// Arithmetic operators (*,/,+,-,%)
+//    if (condition){
+//
+//    }else if (condition){
+//
+//    }else if (condition){
+//
+//    }else{
+//
+//    }
     
-    cout << "Addition : " << variable1 + variable2 << endl;
-    cout << "Multiplication : " << variable1 * variable2 << endl;
-    cout << "Subtraction : " << variable1 - variable2 << endl;
-    cout << "Division : " << variable1 / variable2 << endl;
-    cout << "Remainder : " << variable1 % variable2 << endl;
+    int marks;
     
-    int variable3 = variable1 + variable2;
+    //  marks >= 48 // grade A
+    //  marks >= 38 // grade B
+    //  marks >= 32 // Grade C
+    //  marks >= 24 // Grade D
+    //  marks < 24 // Fail
     
-    cout << "Addition: " << variable3 << endl;
+    cout << "Enter marks : ";
+    ishma:
+    cin >> marks;
+    
+    if (marks > 60){
+        cout << "You entered incorrect marks";
+        cout  << "\nEnter Marks again: ";
+        goto ishma;
+    }
+    
+//    int PTVFee = 35;
+//    
+//    int unit;
+//    int cost_of_electricity;
+//    
+//    if (unit < 100)
+//        cost_of_electricity = unit * 10;
+//    else if(unit >= 100){
+//        int temp = unit - 100;
+//        cost_of_electricity =  100 * 10;
+//        cost_of_electricity = (cost_of_electricity) + (temp * 20);
+//    }
+    
+    
+    if (marks >= 48)
+        cout << "Grade A";
+    else if (marks >= 38)
+        cout << "Grade B";
+    else if (marks >= 32)
+        cout << "Grade C";
+    else if (marks >= 24)
+        cout << "Grade D";
+    else
+        cout << "Fail";
+    
+    
+    
     
     return 0;
 }
